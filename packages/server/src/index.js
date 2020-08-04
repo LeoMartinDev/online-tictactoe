398eedb811637793858e9a26c65df1001eada1fe
leoGeorges <leo@georges.tech>
2020-08-04 22:06:26 +0200

    Fix server not accessible outside docker

--- packages/server/src/index.js
+++ packages/server/src/index.js
@@ -3,7 +3,7 @@ import app from './app/index.js';
 
 const server = app(config);
 
-server.listen(3000, (error) => {
+server.listen(3000, '0.0.0.0', (error) => {
   if (error) {
     server.log.error(error);
     process.exit(1);
